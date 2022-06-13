cfc649c39df3e67201716327bea0197b466845ce
Gongmeda <gongmeda@gmail.com>
2022-06-13 22:39:44 +0900

    feat: products 모듈 CRUD 구현

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => (typeof value == 'string' ? value.trim() : value))
+  @Length(2, 20)
+  name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
+  price: number;
+
+  @IsOptional()
+  @Length(10, 50)
+  description: string;
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,58 @@
+import {
+  Injectable,
+  MethodNotAllowedException,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private sequence = 1;
+  private products: Product[] = [];
+
+  create(createProductDto: CreateProductDto): void {
+    const product = new Product();
+    product.id = this.sequence++;
+    product.name = createProductDto.name;
+    product.price = createProductDto.price;
+    product.description = createProductDto.description;
+    product.isUpdated = false;
+    this.products.push(product);
+  }
+
+  findAll(): Product[] {
+    return this.products;
+  }
+
+  findOne(id: number): Product {
+    const product = this.products.find((product) => product.id === id);
+
+    if (product === undefined) {
+      throw new NotFoundException();
+    }
+
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto): Product {
+    const product = this.findOne(id);
+
+    if (product.isUpdated) {
+      throw new MethodNotAllowedException();
+    }
+
+    product.name = updateProductDto.name ?? product.name;
+    product.price = updateProductDto.price ?? product.price;
+    product.description = updateProductDto.description ?? product.description;
+    product.isUpdated = true;
+    return product;
+  }
+
+  remove(id: number): void {
+    const product = this.findOne(id);
+    this.products.indexOf(product) > -1 &&
+      this.products.splice(this.products.indexOf(product), 1);
+  }
+}

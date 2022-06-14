f3d8301e73c380f6305e3617096189b0962b302c
Gongmeda <gongmeda@gmail.com>
2022-06-14 18:10:42 +0900

    refactor: class2 코드 스켈레톤화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [],
   controllers: [],
   providers: [],
 })

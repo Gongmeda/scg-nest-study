50f65eec510a9429efe539c92ec0246eaf3cf3b0
Gongmeda <gongmeda@gmail.com>
2022-06-18 02:27:03 +0900

    refactor: class2 코드 스켈레톤화 (재구성)

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,23 +1,5 @@
-import {
-  IsInt,
-  IsNotEmpty,
-  IsOptional,
-  IsPositive,
-  IsString,
-  Length,
-} from 'class-validator';
-
 export class CreateProductDto {
-  @IsNotEmpty()
-  @IsString()
-  @Length(2, 20)
   name: string;
-
-  @IsNotEmpty()
-  @IsInt()
-  @IsPositive()
   price: number;
-
-  @IsOptional()
   description: string;
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,11 +1,6 @@
-import { Exclude } from 'class-transformer';
-
 export class Product {
   id: number;
   name: string;
   price: number;
   description: string;
-
-  @Exclude()
-  isUpdated: boolean;
 }
